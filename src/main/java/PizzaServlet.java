8c28bfe7994ff975e07609d5d1b2a8c601114515
Austin Hunt <austin.hunt.012@gmail.com>
2023-02-03 10:50:57 -0600

    fix bug

--- src/main/java/PizzaServlet.java
+++ src/main/java/PizzaServlet.java
@@ -6,19 +6,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("pizza-order")
+@WebServlet(name = "PizzaServlet", urlPatterns = "/pizza-order")
 public class PizzaServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String crust = request.getParameter("crust");
-        String sauce = request.getParameter("sauce");
-        String size = request.getParameter("size");
-        String[] toppings = request.getParameterValues("toppings");
-        String address = request.getParameter("address");
 
-        System.out.println("Crust: " + crust);
-        System.out.println("Sauce: " + sauce);
-        System.out.println("Size: " + size);
-        System.out.println("Toppings: " + Arrays.toString(toppings));
-        System.out.println("Address: " + address);
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/pizza-order.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println(request.getParameter("crust"));
+
     }
 }
+
